6433959652b5f8b7d27feeb3da233465f3c01249
Josh <josh@eastfive.com>
2017-01-09 09:32:30 -0600

    merged

--- Helpers/HttpActionHelpers.cs
+++ Helpers/HttpActionHelpers.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using BlackBarLabs.Extensions;
 
 namespace BlackBarLabs.Api.Tests
 {
--- Sessions/TestSession.cs
+++ Sessions/TestSession.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using BlackBarLabs.Api.Services;
 using BlackBarLabs.Web;
-using Microsoft.WindowsAzure;
 
 namespace BlackBarLabs.Api.Tests
 {
