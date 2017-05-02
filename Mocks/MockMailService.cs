1e0d5c2741e7fabd11c1f1effa60b890398b5779
Josh <josh@eastfive.com>
2017-05-02 13:19:29 -0500

    new template mailing method

--- Mocks/MockMailService.cs
+++ Mocks/MockMailService.cs
@@ -7,17 +7,18 @@ namespace BlackBarLabs.Api.Tests
 {
     public class MockMailService : EastFive.Api.Services.ISendMessageService
     {
-        public delegate Task SendEmailMessageDelegate(string toAddress, string toName, string fromAddress, string fromName,
-            string templateName, IDictionary<string, string> substitutionsSingle, IDictionary<string, string[]> substitutionsMultiple);
+        public delegate Task SendEmailMessageDelegate(string templateName, string toAddress, string toName, string fromAddress, string fromName,
+            string subject, IDictionary<string, string> substitutionsSingle);
 
         public SendEmailMessageDelegate SendEmailMessageCallback { get; set; }
         
-        public async Task<TResult> SendEmailMessageAsync<TResult>(string toAddress, string toName, string fromAddress, string fromName,
-            string templateName, IDictionary<string, string> substitutionsSingle, IDictionary<string, string[]> substitutionsMultiple, Func<string, TResult> onSuccess, Func<TResult> onServiceUnavailable, Func<string, TResult> onFailed)
+        public async Task<TResult> SendEmailMessageAsync<TResult>(string templateName, string toAddress, string toName, string fromAddress, string fromName,
+            string subject, IDictionary<string, string> substitutionsSingle,
+            Func<string, TResult> onSuccess, Func<TResult> onServiceUnavailable, Func<string, TResult> onFailed)
         {
-            await this.SendEmailMessageCallback.Invoke(
+            await this.SendEmailMessageCallback.Invoke(templateName,
                 toAddress, toName, fromAddress, fromName,
-                templateName, substitutionsSingle, substitutionsMultiple);
+                subject, substitutionsSingle);
             return onSuccess(Guid.NewGuid().ToString());
         }
     }
