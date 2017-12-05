45b0878c3faa0d46710e4dbc53c24602d07de536
Josh <josh@eastfive.com>
2017-12-05 08:16:02 -0600

    updated mocks to match interface chagnes

--- Mocks/ProvideLoginMock.cs
+++ Mocks/ProvideLoginMock.cs
@@ -57,47 +57,6 @@ namespace EastFive.Api.Tests
 
             return onSuccess(userId, stateId, default(Guid?), tokensFromResponse).ToTask();
         }
-
-        public Uri GetLoginUrl(string redirect_uri, byte mode, byte[] state, Uri responseControllerLocation)
-        {
-            return (new Uri("http://example.com/login"))
-                .AddQuery("redirect", redirect_uri)
-                .AddQuery("mode", ((int)mode).ToString())
-                .AddQuery("data", Convert.ToBase64String(state))
-                .AddQuery("state", GetState(redirect_uri, mode, state));
-        }
-
-        public Uri GetSignupUrl(string redirect_uri, byte mode, byte[] state, Uri responseControllerLocation)
-        {
-            return (new Uri("http://example.com/signup"))
-                .AddQuery("redirect", redirect_uri)
-                .AddQuery("mode", ((int)mode).ToString())
-                .AddQuery("data", Convert.ToBase64String(state))
-                .AddQuery("state", GetState(redirect_uri, mode, state));
-        }
-
-        public Uri GetLogoutUrl(string redirect_uri, byte mode, byte[] state, Uri responseControllerLocation)
-        {
-            return (new Uri("http://example.com/logout"))
-                .AddQuery("redirect", redirect_uri)
-                .AddQuery("mode", ((int)mode).ToString())
-                .AddQuery("data", Convert.ToBase64String(state))
-                .AddQuery("state", GetState(redirect_uri, mode, state));
-        }
-
-        private string GetState(string redirect_uri, byte mode, byte[] state)
-        {
-            var redirBytes = System.Text.Encoding.ASCII.GetBytes(redirect_uri);
-            var stateBytes = (new byte[][]
-            {
-                BitConverter.GetBytes(((short)redirBytes.Length)),
-                redirBytes,
-                new byte [] {mode},
-                state,
-            }).SelectMany().ToArray();
-            var base64 = Convert.ToBase64String(stateBytes);
-            return base64;
-        }
         
         public static string GetToken(string userId)
         {
@@ -105,41 +64,27 @@ namespace EastFive.Api.Tests
             tokens.Add(token, userId);
             return token;
         }
-
-        public TResult ParseState<TResult>(string state,
-            Func<Uri, byte, byte[], TResult> onSuccess,
-            Func<string, TResult> invalidState)
-        {
-            var bytes = Convert.FromBase64String(state);
-            var urlLength = BitConverter.ToInt16(bytes, 0);
-            if (bytes.Length < urlLength + 3)
-                return invalidState("Encoded redirect length is invalid");
-            var addr = System.Text.Encoding.ASCII.GetString(bytes, 2, urlLength);
-            Uri url;
-            if (!Uri.TryCreate(addr, UriKind.RelativeOrAbsolute, out url))
-                return invalidState($"Invalid value for redirect url:[{addr}]");
-            var mode = bytes.Skip(urlLength + 2).First();
-            var data = bytes.Skip(urlLength + 3).ToArray();
-            return onSuccess(url, mode, data);
-        }
         
-        public Task DeleteAuthorizationAsync(Guid loginId)
-        {
-            //throw new NotImplementedException();
-            return 1.ToTask();
-        }
 
-        public TResult ParseState<TResult>(string state,
-            Func<byte, byte[], IDictionary<string, string>, TResult> onSuccess, Func<string, TResult> invalidState)
+        public Uri GetLoginUrl(Guid state, Uri responseControllerLocation)
         {
-            return onSuccess(0, new byte[] { }, new Dictionary<string, string>());
-        }
-
-        public Uri GetLoginUrl(Guid state, Uri redirectUri)
-        {
-            return new Uri("http://provideloginmock.example.com")
+            return new Uri("http://provideloginmock.example.com/login")
                 .AddQuery(ProvideLoginMock.extraParamState, state.ToString())
-                .AddQuery("redirect", redirectUri.AbsoluteUri);
+                .AddQuery("redirect", responseControllerLocation.AbsoluteUri);
+        }
+
+        public Uri GetLogoutUrl(Guid state, Uri responseControllerLocation)
+        {
+            return new Uri("http://provideloginmock.example.com/logout")
+                .AddQuery(ProvideLoginMock.extraParamState, state.ToString())
+                .AddQuery("redirect", responseControllerLocation.AbsoluteUri);
+        }
+
+        public Uri GetSignupUrl(Guid state, Uri responseControllerLocation)
+        {
+            return new Uri("http://provideloginmock.example.com/signup")
+                .AddQuery(ProvideLoginMock.extraParamState, state.ToString())
+                .AddQuery("redirect", responseControllerLocation.AbsoluteUri);
         }
     }
 
