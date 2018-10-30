a98546bd3418b8f7f3933ff81f8bbe8599c596dc
Josh Wingstrom <josh@eastfive.com>
2018-10-30 11:25:32 -0500

    work in progress on refactor of FVC instgator and validator handling

---
+++ References/ReferenceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api.Tests
+{
+    public static class ReferenceExtensions
+    {
+        public static IRefs<TType> AsRefs<TType>(this Guid [] guids)
+        {
+            return new TestRefs<TType>(guids);
+        }
+    }
+}
---
+++ References/TestRefs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EastFive.Linq.Async;
+
+namespace EastFive.Api.Tests
+{
+    public class TestRefs<TType> : IRefs<TType>
+    {
+        public TestRefs(Guid[] ids)
+        {
+            this.ids = ids;
+        }
+
+        public Guid[] ids
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerableAsync<TType> Values => throw new NotImplementedException();
+    }
+}
