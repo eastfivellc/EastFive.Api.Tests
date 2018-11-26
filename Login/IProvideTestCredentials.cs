d08118ba36cf7f7ae87877aef004e458dd5f5126
Josh Wingstrom <josh@eastfive.com>
2018-11-25 23:23:15 -0600

    new login mocks

---
+++ Login/IProvideTestCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Api.Tests
+{
+    public interface IProvideTestCredentials
+    {
+        Task<IDictionary<string, string>> ProvideCredentialsAsync();
+    }
+}
